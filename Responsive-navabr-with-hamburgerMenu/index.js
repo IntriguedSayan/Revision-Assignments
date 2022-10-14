45a5f81b306749872b238aaf84cfc860fa0f45db
IntriguedSayan <sayan.mid590@gmail.com>
2022-10-14 23:32:16 +0530

    responsive navbar bug fixed

--- Responsive-navabr-with-hamburgerMenu/index.js
+++ Responsive-navabr-with-hamburgerMenu/index.js
@@ -1,8 +1,20 @@
 
+const menu=document.getElementById("initHamburger")
+
+window.addEventListener("resize",()=>{
+
+    const width=window.innerWidth
+    if(Number(width)>720){
+        menu.style.display="flex"
+    }else{
+        menu.style.display="none"
+    }
+
+})
 
 function openMenu(){
 
-    const menu=document.getElementById("initHamburger")
+   
     menu.style.display==="none"?menu.style.display="flex":menu.style.display="none"
 
 }
